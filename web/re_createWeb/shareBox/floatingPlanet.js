0a71012e4320981c89854f664bdcb04081714a5f
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-28 18:00:46 +0900

    Add files via upload

---
+++ web/re_createWeb/shareBox/floatingPlanet.js
@@ -0,0 +1,9 @@
+function creatingPlanet(){
+    let code="";
+    code +="<div>sfsfsefsf</div>";
+    document.getElementById("planet").innerHTML=code;
+}
+
+function brighten(){
+    alert("a");
+}
