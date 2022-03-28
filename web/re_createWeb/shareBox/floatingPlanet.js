2592962ef96f7a91b29609b9dec100854b29664e
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-28 18:00:16 +0900

    Delete web directory

--- web/re_createWeb/shareBox/floatingPlanet.js
+++
@@ -1,9 +0,0 @@
-function creatingPlanet(){
-    let code="";
-    code +="<div>sfsfsefsf</div>";
-    document.getElementById("planet").innerHTML=code;
-}
-
-function brighten(){
-    alert("a");
-}
