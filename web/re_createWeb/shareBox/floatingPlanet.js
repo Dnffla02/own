99f708e9d8db33dc9006c3d668046766183fa8e3
blaRe <96527175+Dnffla02@users.noreply.github.com>
2022-03-29 13:30:09 +0900

    Update floatingPlanet.js

--- web/re_createWeb/shareBox/floatingPlanet.js
+++ web/re_createWeb/shareBox/floatingPlanet.js
@@ -1,9 +1,11 @@
 function creatingPlanet(){
     let code="";
-    code +="<div>sfsfsefsf</div>";
+    code +="<div id=#???>sfsfsefsf</div>";
     document.getElementById("planet").innerHTML=code;
 }
 
 function brighten(){
     alert("a");
 }
+
+채ㅜㄴㅅ 
